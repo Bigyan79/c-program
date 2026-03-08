fa6601fe75656331895109b2d7e22e8e81311212
Bigyan <bigyan.ojha1@gmail.com>
2026-03-09 00:29:20 +0545

    feat: initialize winsock api

---
+++ src/server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <winsock2.h> 
+
+int main()
+{
+    struct WSAData wsaData;
+
+    // Initialize WinSock API
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        printf("Failed to startup WSA\n");
+        return 1;
+    }
+    return 0;
+}
